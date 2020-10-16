591e39f40c4b96e49266a4652fa4fd2a8ed1424e
raquelribeiro2 <raqu3l.rodriguesribeiro@gmail.com>
2020-10-16 18:35:27 -0300

    Route navigation

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
+  const { params } = useRouteMatch<RepositoryParams>();
+
   return <h1>Repository</h1>;
 };
 
